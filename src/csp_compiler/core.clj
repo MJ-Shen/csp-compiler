9f77ba3ba219029d989e751fe67db8e20898e9cc
MJ-Shen <smj2009@126.com>
2015-02-12 17:57:22 +0800

    namespace-invoke
    modify in namespace and to invoke

--- src/csp_compiler/core.clj
+++ src/csp_compiler/core.clj
@@ -4,6 +4,7 @@
 	(:require [clojure.java.io :as jio])
 	(:import [java.io File]))
 
+(def *csp-ns-map* (ref {}))
 
 (defn read-file [path option]
 	(let [content (slurp path :encoding (:encoding option))]
@@ -19,15 +20,32 @@
 		 :ns (.replaceAll (.replaceAll op ".\\w+$" "") (str fs) ".")}
 		))
 
+(defn invoke
+	"call out (function) if no changes happen"
+	[^String ns]
+	(binding [*ns* (the-ns ns)]
+		(println *ns* (eval 'out))
+		((eval 'out))))
+
+(defn if-recompile
+	[csp-meta]
+	(cond
+		(:force-sync option*) true
+		(:read-time csp-meta)))
+
 (defn process
 	[path option*]
 	(println (str option*))
-	(let [content (read-file path option*) csp-meta (parse path)]
+	(let [csp-meta (parse path)]
+		if())
+	(let [content (read-file path option*) ]
 		(write-file csp-meta content option*)))
 
 (defn csp
 	"cps  call the clojure server page (also created or update if necessary)
-	:option [:encoding :auto-sync]"
+	:option [:encoding UTF-8 :auto-sync true :force-sync false]"
 	[path & op]
 	(def option* (merge {:encoding "UTF-8"} (first op)))
 	(process path option*))
+
+
