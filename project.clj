04c831788151622d7478abcbbeb29622b00cd719
MJ-Shen <smj2009@126.com>
2015-03-27 14:12:32 +0800

    update profile
    update profile

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject csp-compiler "0.1.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Compiler for clojure server page"
+  :url "https://github.com/MJ-Shen/csp-compiler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                                   :creds :gpg}
+                       "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
+                                    :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
   							 [me.raynes/fs "1.4.6"]])
