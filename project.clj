6f7c815c6e772c16716cda44e955c165dde7faa5
MJ-Shen <smj2009@126.com>
2015-08-11 18:21:27 +0800

    ~
    ~

--- project.clj
+++ project.clj
@@ -3,9 +3,7 @@
   :url "https://github.com/MJ-Shen/csp-compiler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                                   :creds :gpg}
-                       "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                    :creds :gpg}]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                            :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
   							 [me.raynes/fs "1.4.6"]])
